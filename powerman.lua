b9260301824938f811c01095b625b46814e62daf
finalneuron <dijpak@gmail.com>
2026-03-12 20:20:59 -0500

    Updated from lappy.

--- powerman.lua
+++ powerman.lua
@@ -1,14 +1,29 @@
-declare isCharging flag
-declare MINCHARGE constant
-declare MAXCHARGE constant
-declare storedPow var
+--powerman.lua
+--Manages the BigReactor to maintain proper energy
+--storage charge levels.
+
+isRunning = false
+MINCHARGE = 20	--Percentage
+MAXCHARGE = 80	--Percentage
+powerPerc = 0	--Percentage
+
+compStorage = --power storage device
+compReactor = --reactor device
 
 loop
-  read storedPow
-  if storedPow < minCharge and !isCharging
-    turn on reactor
-    isCharging = true
-  else if storedPow > maxCharge and isCharging
-    turn off reactor
-    isCharging = false
+	powerPerc = getPowerPerc()
+	if powerPerc < MINCHARGE and !isRunning
+    isRunning = true
+--	compReactor.power(on)???
+	else if powerPerc > MAXCHARGE and isRunning
+    isRunning = false
+--	compReactor.power(on)???
+	end
+end
+
+getPowerPerc()
+--return stored power as a percentage of total
+--	pow = --method to get the stored power
+--	max = --method to get max storable power
+--	return (round(pow/max))
 end
