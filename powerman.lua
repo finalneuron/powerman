c88bc26a3d092bd4baf175a2bf46e190708e5ff5
jim <dijpak@gmail.com>
2026-03-14 16:05:30 -0500

    Corrected ordering of function declaration and some syntax.

--- powerman.lua
+++ powerman.lua
@@ -3,28 +3,34 @@
 --storage charge levels.
 
 local isRunning = false
-local MINCHARGE = 20	--Percentage
-local MAXCHARGE = 80	--Percentage
-local powerPerc = 0		--Percentage
+local MINCHARGE = 20         --Percentage
+local MAXCHARGE = 80         --Percentage
+local powerPerc = 0          --Percentage
 
 local component = require("component")
-local compStorage = component.capacitor_bank
-local compReactor = component.br_reactor
-
-while true do
-	powerPerc = getPowerPerc()
-	if powerPerc < MINCHARGE and !isRunning
-    isRunning = true
-	compReactor.setActive(isRunning)
-	else if powerPerc > MAXCHARGE and isRunning
-    isRunning = false
-	compReactor.setActive(isRunning)
-	end
-end
+compStorage = component.tile_blockcapacitorbank_name
+compReactor = component.br_reactor
 
 --return stored power as a percentage of total
 function getPowerPerc()
-	pow = compStorage.getEnergyStored() --function to get the stored power
-	max = compStorage.getMaxEnergyStored() --function to get max storable power
-	return (pow/max*100)
+  pow = compStorage.getEnergyStored() --function to get the stored power
+  print("Got stored")
+  max = compStorage.getMaxEnergyStored() --function to get max storable power
+  print("Got max")
+  val = pow / max * 100
+  return val
+end
+
+while true do
+  powerPerc = getPowerPerc()
+  if powerPerc < MINCHARGE and not isRunning then
+    isRunning = true
+    compReactor.setActive(isRunning)
+    print("Running")
+  elseif powerPerc > MAXCHARGE and isRunning then
+    isRunning = false
+    compReactor.setActive(isRunning)
+    print("Off")
+  end
+  os.sleep(5)
 end
